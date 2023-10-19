095b679e42cd5107f4f7b88405a48110a55e5e4f
Wiktor <w.zawadzki@mail.com>
2023-10-19 21:51:42 +0200

    code update

--- INC/x00_pointers/x02_Queue.c
+++ INC/x00_pointers/x02_Queue.c
@@ -1,6 +1,29 @@
 #include "x02_Queue.h"
 #ifdef _X02_INCLUDE_QUEUE_H
 
+/****************************************************************************************************
+ * @name    
+ * @brief   
+ * 
+ * @param[in]    
+ * @param[in]   
+ * 
+ * @note           
+ * @return      
+*/
+//===================================================================================================
+
+/****************************************************************************************************
+ * @name        _MoveData
+ * @brief       Move any type of data from to destination pointer
+ * 
+ * @param[in]   pDST Destination pointer  
+ * @param[in]   pSRC Source pointer 
+ * @param[in]   dataLenght Lenght of data 
+ * 
+ * @note        Staric function
+ * @return      void
+*/
 static void _MoveData(char* pDST, char* pSRC, int dataLenght)
 {
     int i;
@@ -9,7 +32,18 @@ static void _MoveData(char* pDST, char* pSRC, int dataLenght)
         *(pDST + i) = *(pSRC + i);
     }
 }
+//===================================================================================================
 
+/****************************************************************************************************
+ * @name        QueueInit
+ * @brief       Init function for Queue
+ * 
+ * @param[in]   pQueue Pointer of type queueMenagment_t* for Queue menagment global data struture.
+ * @param[in]   pElem Pointer of type elm_data_t* for input first element to queue
+ * 
+ * @note           
+ * @return      Status of operation of type char
+*/
 char QueueInit(queueMenagment_t* pQueue,   elm_data_t* pElem)
 {
     queueEl_t* pNew = (queueEl_t*)malloc(sizeof(queueEl_t));
@@ -24,11 +58,11 @@ char QueueInit(queueMenagment_t* pQueue,   elm_data_t* pElem)
 
     return STATUS_SUCES;
 }
+//===================================================================================================
 
-
-/**********************************************************************
- * @name    QueuePut
- * @brief   Add another element to the top of the head
+/****************************************************************************************************
+ * @name        QueuePut
+ * @brief       Add another element to the top of the head
  * 
  * @param[in]   pQueue Pointer to the menagment structure 
  * @param[in]   pElem Pointer to the new element 
@@ -55,7 +89,18 @@ char QueuePut(queueMenagment_t* pQueue,     elm_data_t* pElem)
     else
         return STATUS_ERROR;
 }
+//===================================================================================================
 
+/****************************************************************************************************
+ * @name        QueueGet
+ * @brief       Get element from head of the queue
+ * 
+ * @param[in]   pQueue Pointer to the menagment structure 
+ * @param[in]   pElem Pointer to return element data
+ * 
+ * @note           
+ * @return      Status of the operation
+*/
 char QueueGet(queueMenagment_t* pQueue,     elm_data_t* pElem)
 {
     if(pQueue->lAmount >= QUEUE_ONE_ELEMENT)
@@ -66,7 +111,18 @@ char QueueGet(queueMenagment_t* pQueue,     elm_data_t* pElem)
     else    
         return STATUS_ERROR;
 }
+//===================================================================================================
 
+/****************************************************************************************************
+ * @name        QueueRemove
+ * @brief       Get element from head of the queue and remove
+ * 
+ * @param[in]   pQueue Pointer to the menagment structure
+ * @param[in]   pElem Pointer to return element data
+ * 
+ * @note           
+ * @return      Status of the operation
+*/
 char QueueRemove(queueMenagment_t* pQueue,  elm_data_t* pElem)
 {
     if(pQueue->lAmount == QUEUE_EMPTY)
@@ -87,5 +143,6 @@ char QueueRemove(queueMenagment_t* pQueue,  elm_data_t* pElem)
         return STATUS_SUCES;
     }
 }
+//===================================================================================================
 
 #endif // _X02_INCLUDE_QUEUE_H
